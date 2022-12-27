2e22c52b1b7e791cf8902e471f46bb49f05a3ccc
polina <polina.zdanovich.02@mail.ru>
2022-12-27 22:32:34 +0300

    daily

--- src/service/service.user.js
+++ src/service/service.user.js
@@ -18,19 +18,19 @@ async function createUser(name, surname, birth, city, age) {
     return user
 }
 
-async function updateUser(info_id, name, surname, birth, city, age){
+async function updateUser(info_id, name, surname, birth, city, age) {
     const user = await updateUserDB(info_id, name, surname, birth, city, age)
     if (!user.length) throw new Error("not found")
     return user
 }
 
-async function deleteUser(id){
+async function deleteUser(id) {
     const user = await deleteUserDB(id)
     if (!user.length) throw new Error("not found")
     return user
 }
 
-async function patchUser(id, dataFromClient){
+async function patchUser(id, dataFromClient) {
     const user = await patchUserDB(id, dataFromClient)
     if (!user.length) throw new Error("not found")
     return user
