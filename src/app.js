4770a1528db824c48150a35436960e493380ce0e
polina <polina.zdanovich.02@mail.ru>
2022-12-28 22:51:37 +0300

    daily

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const user = require("./controller/controller.user")
+
 const app = express()
 
 app.use(bodyParser.json())
