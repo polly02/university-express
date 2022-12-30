8aff70b6f7d192105d343f1da3be61373be0b02a
polina <polina.zdanovich.02@mail.ru>
2022-12-30 22:17:54 +0300

    d

--- src/helper/validation.js
+++ src/helper/validation.js
@@ -19,10 +19,10 @@ async function isValidBody(req, res, next) {
     if (!name) throw new Error(ExceptionType.NAME_NOT_FOUND.message)
     if (!surname) throw new Error(ExceptionType.SURNAME_NOT_FOUND.message)
     if (!birth) throw new Error(ExceptionType.BIRTH_NOT_FOUND.message)
-    if(!/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/g.test(birth)) throw new Error(ExceptionType.BIRTH_NOT_VALID.message)
+    if (!/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/g.test(birth)) throw new Error(ExceptionType.BIRTH_NOT_VALID.message)
     if (!city) throw new Error(ExceptionType.CITY_NOT_FOUND.message)
     if (!age) throw new Error(ExceptionType.AGE_NOT_FOUND.message)
-    if(isNaN(age)) throw new Error(ExceptionType.AGE_NOT_VALID.message)
+    if (isNaN(age)) throw new Error(ExceptionType.AGE_NOT_VALID.message)
     next()
 }
 
